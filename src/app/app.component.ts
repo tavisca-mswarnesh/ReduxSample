0b7c9da4b399458e3d8d1ceef32be62ee86c7136
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-12-13 16:27:01 +0530

    Phase two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,61 @@
-import { Component } from '@angular/core';
-import { NgRedux, select } from '@angular-redux/store';
-import { Observable } from 'rxjs';
+import { Component } from "@angular/core";
+import { NgRedux, select } from "@angular-redux/store";
+import { Observable } from "rxjs";
 
-import { IAppState } from './store/index';
-import { SUBMIT ,ISFOCUSEDFIRSTNAME,ISFOCUSEDLASTNAME} from './actions/counter.actions';
-let temp:any;
+import { IAppState } from "./store/index";
+import {
+  SUBMITFORM1,
+  SUBMITFORM2,
+  ISFOCUSEDFIRSTNAME1,
+  ISFOCUSEDLASTNAME1,
+  ISFOCUSEDFIRSTNAME2,
+  ISFOCUSEDLASTNAME2
+} from "./actions/counter.actions";
+let temp: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ReduxSample';
-  @select('counter')  public counter$: Observable<any>;
-  
-  constructor(private ngRedux: NgRedux<IAppState>) { }
-  
-  ngOnInit(){
-    var temp;
-    this.counter$.subscribe(e=>temp=e)
-    this.fname=temp.value1.value,
-    this.lname=temp.value2.value
-  }
-  fname:string;
-  lname:string;
-  submit(){
-    this.ngRedux.dispatch({ type: SUBMIT,data:{ fName:this.fname,lName:this.lname}});
-  }
-  isFocusedFirstName(status:boolean){
-    this.ngRedux.dispatch({type:ISFOCUSEDFIRSTNAME,data:{status}})
-  }
-  isFocusedLastName(status:boolean)
-  {
-    this.ngRedux.dispatch({type:ISFOCUSEDLASTNAME,data:{status}})
+  title = "ReduxSample";
+  @select("counter") public counter$: Observable<any>;
 
+  constructor(private ngRedux: NgRedux<IAppState>) {}
+
+  ngOnInit() {
+    var temp;
+    this.counter$.subscribe(e => (temp = e));
+    (this.fnameForm1 = temp.value1.fName.value), (this.lnameForm1 = temp.value1.lName.value),
+    (this.fnameForm2 = temp.value2.fName.value), (this.lnameForm2 = temp.value2.lName.value);
   }
-  
+  fnameForm1: string;
+  lnameForm1: string;
+  fnameForm2: string;
+  lnameForm2: string;
+  submitForm1() {
+    this.ngRedux.dispatch({
+      type: SUBMITFORM1,
+      data: { fName: this.fnameForm1, lName: this.lnameForm1 }
+    });
+  }
+  submitForm2() {
+    this.ngRedux.dispatch({
+      type: SUBMITFORM2,
+      data: { fName: this.fnameForm2, lName: this.lnameForm2 }
+    });
+  }
+  isFocusedFirstNameInForm1(status: boolean) {
+    this.ngRedux.dispatch({ type: ISFOCUSEDFIRSTNAME1, data: { status } });
+  }
+  isFocusedLastNameInForm1(status: boolean) {
+    this.ngRedux.dispatch({ type: ISFOCUSEDLASTNAME1, data: { status } });
+  }
+  isFocusedFirstNameInForm2(status: boolean) {
+    this.ngRedux.dispatch({ type: ISFOCUSEDFIRSTNAME2, data: { status } });
+  }
+  isFocusedLastNameInForm2(status: boolean) {
+    this.ngRedux.dispatch({ type: ISFOCUSEDLASTNAME2, data: { status } });
+  }
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,32 +1,66 @@
-import { SUBMIT,ISFOCUSEDFIRSTNAME,ISFOCUSEDLASTNAME } from '../actions/counter.actions';
-import {formProperties} from '../model/formProperties'
-import { from } from 'rxjs';
- let fName:formProperties={value:"Hello",isFocused:false,isTouched:false}
- let lName:formProperties={value:"world",isFocused:false,isTouched:false}
+import {
+  SUBMITFORM1,
+  SUBMITFORM2,
+  ISFOCUSEDFIRSTNAME1,
+  ISFOCUSEDLASTNAME1,
+  ISFOCUSEDFIRSTNAME2,
+  ISFOCUSEDLASTNAME2
+} from "../actions/counter.actions";
+import { formProperties } from "../model/formProperties";
+import { from } from "rxjs";
+let fName: formProperties = {
+  value: "Hello",
+  isFocused: false,
+  isTouched: false
+};
+let lName: formProperties = {
+  value: "world",
+  isFocused: false,
+  isTouched: false
+};
+let fName2: formProperties = {
+  value: "I am",
+  isFocused: false,
+  isTouched: false
+};
+let lName2: formProperties = {
+  value: "swar",
+  isFocused: false,
+  isTouched: false
+};
+let form1={fName:fName,lName:lName}
+let form2={fName:fName2,lName:lName2}
 
-const INITIAL_STATE = { "value1": fName ,"value2":lName};
+const INITIAL_STATE = { value1: form1, value2: form2 };
 
 export function CounterReducer(state = INITIAL_STATE, action: any): any {
   console.log(action.type);
   switch (action.type) {
-    case SUBMIT:
-      state.value1.value=action.data.fName;
-      state.value2.value=action.data.lName;
+    case SUBMITFORM1:
+      state.value1.fName.value = action.data.fName;
+      state.value1.lName.value = action.data.lName;
       break;
-    case ISFOCUSEDFIRSTNAME:
-      state.value1.isTouched=true;
-      //state.value1.isFocused=!state.value1.isFocused;action.data
-      state.value1.isFocused=action.data.status;
+    case SUBMITFORM2:
+      state.value2.fName.value = action.data.fName;
+      state.value2.lName.value = action.data.lName;
       break;
-    case ISFOCUSEDLASTNAME:
-      state.value2.isTouched=true;
-      state.value1.isFocused=action.data.status;
-
-        break;
+    case ISFOCUSEDFIRSTNAME1:
+      state.value1.fName.isTouched = true;
+      state.value1.fName.isFocused = action.data.status;
+      break;
+    case ISFOCUSEDLASTNAME1:
+      state.value1.lName.isTouched = true;
+      state.value1.lName.isFocused = action.data.status;
+    case ISFOCUSEDFIRSTNAME2:
+      state.value2.fName.isTouched = true;
+      state.value2.fName.isFocused = action.data.status;
+      break;
+    case ISFOCUSEDLASTNAME2:
+      state.value2.lName.isTouched = true;
+      state.value2.lName.isFocused = action.data.status;
+      break;
     default:
       break;
-    
   }
   return state;
 }
-
